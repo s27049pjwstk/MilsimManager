0fd4354a4ac2940938ff979cebc4a5ea2f430bb5
Puotek <hubertosz.p@gmail.com>
2026-02-10 12:45:15 +0100

    Placeholder Image Extension

---
+++ MilsimManager/Extensions/PlaceholderImageExtensions.cs
@@ -0,0 +1,5 @@
+﻿namespace MilsimManager.Extensions;
+
+public static class PlaceholderImageExtensions {
+    public static string GetImageUrl(string seed, int width = 1000, int? height = null) => $"https://picsum.photos/seed/{seed}/{width}" + (height is null ? string.Empty : $"/{height}");
+}
